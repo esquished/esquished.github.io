b4fffe9b6d2d70c79085d416edcf641f929422d2
esquished <omni@PRGM.local>
2018-10-08 15:12:13 -0500

    initial

---
+++ scripts/audioPlayer.js
@@ -0,0 +1,16 @@
+var ap1 = new APlayer({
+    element: document.getElementById('player1'),
+    narrow: false,
+    autoplay: false,
+    showlrc: 0,
+    mutex: true,
+    theme: '#ff2828',
+    music: {
+            title: 'WHAT',
+            author: 'SQUISHED',
+            url: 'tunes/tease_01.mp3',
+            pic: 'img/covers/randCover.jpg'
+        }
+    
+});
+
